deeac334da9af0b1fe1563c5da0c173def80c7bb
M4r-ku5 <138504497+M4r-ku5@users.noreply.github.com>
2025-07-08 21:44:56 +0200

    Update Popolamento.sql

--- Database/Popolamento.sql
+++ Database/Popolamento.sql
@@ -12,16 +12,16 @@ INSERT INTO gate (numero) VALUES (4);
 
 -- Popolamento della tabella VOLO
 INSERT INTO volo (codice, compagnia_aerea, data, orario, stato_volo, tipo_volo, aeroporto_origine, aeroporto_destinazione, idGateFK)
-VALUES ('AZ101', 'Alitalia', '2025-08-05', '10:30:00', 'In Orario', 'Partenza', 'Napoli', 'Roma', 1);
+VALUES ('AZ101', 'Alitalia', '2025-08-05', '10:30:00', 'Programmato', 'Partenza', 'Napoli', 'Roma', 1);
 
 INSERT INTO volo (codice, compagnia_aerea, data, orario, stato_volo, tipo_volo, aeroporto_origine, aeroporto_destinazione, idGateFK)
-VALUES ('LH202', 'Lufthansa', '2025-08-06', '14:00:00', 'In Orario', 'Partenza', 'Napoli', 'Francoforte', 2);
+VALUES ('LH202', 'Lufthansa', '2025-08-06', '14:00:00', 'Programmato', 'Partenza', 'Napoli', 'Francoforte', 2);
 
 INSERT INTO volo (codice, compagnia_aerea, data, orario, stato_volo, tipo_volo, aeroporto_origine, aeroporto_destinazione, idGateFK)
 VALUES ('BA303', 'British Airways', '2025-08-07', '08:45:00', 'In Ritardo', 'Partenza', 'Napoli', 'Londra', 3);
 
 INSERT INTO volo (codice, compagnia_aerea, data, orario, stato_volo, tipo_volo, aeroporto_origine, aeroporto_destinazione, idGateFK)
-VALUES ('AF404', 'Air France', '2025-08-05', '12:00:00', 'In Orario', 'Arrivo', 'Parigi', 'Napoli', NULL);
+VALUES ('AF404', 'Air France', '2025-08-05', '12:00:00', 'Programmato', 'Arrivo', 'Parigi', 'Napoli', NULL);
 
 INSERT INTO volo (codice, compagnia_aerea, data, orario, stato_volo, tipo_volo, aeroporto_origine, aeroporto_destinazione, idGateFK)
 VALUES ('RY505', 'Ryanair', '2025-08-06', '16:15:00', 'Cancellato', 'Partenza', 'Napoli', 'Barcellona', 4);

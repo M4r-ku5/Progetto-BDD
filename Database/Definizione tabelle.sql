26fc962643461ffc6a664cedcb045e57561419a0
M4r-ku5 <138504497+M4r-ku5@users.noreply.github.com>
2025-07-08 21:45:26 +0200

    Update Definizione tabelle.sql

--- Database/Definizione tabelle.sql
+++ Database/Definizione tabelle.sql
@@ -1,11 +1,10 @@
 -- Definizione della tabella UTENTE
 CREATE TABLE utente (
   idUtente INTEGER PRIMARY KEY,
-  login VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  ruolo VARCHAR(50) NOT NULL
+  login VARCHAR(100) NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  ruolo VARCHAR(100) NOT NULL
 );
-
 
 -- Definizione della tabella GATE
 CREATE TABLE gate (
@@ -13,34 +12,31 @@ CREATE TABLE gate (
   numero INTEGER NOT NULL
 );
 
-
 -- Definizione della tabella PRENOTAZIONE
 CREATE TABLE prenotazione (
   idPrenotazione INTEGER PRIMARY KEY,
-  nome_passeggero VARCHAR(255) NOT NULL,
-  numero_biglietto VARCHAR(255) NOT NULL,
-  posto_assegnato VARCHAR(255) NOT NULL,
-  stato_prenotazione VARCHAR(50) NOT NULL,
+  nome_passeggero VARCHAR(100) NOT NULL,
+  numero_biglietto VARCHAR(100) NOT NULL,
+  posto_assegnato VARCHAR(100) NOT NULL,
+  stato_prenotazione VARCHAR(100) NOT NULL,
   idUtenteFK INTEGER NOT NULL,
   idVoloFK INTEGER NOT NULL
 );
 
-
 -- Definizione della tabella VOLO
 CREATE TABLE volo (
   idVolo INTEGER PRIMARY KEY,
-  codice VARCHAR(255) NOT NULL,
-  compagnia_aerea VARCHAR(255) NOT NULL,
+  codice VARCHAR(100) NOT NULL,
+  compagnia_aerea VARCHAR(100) NOT NULL,
   data DATE NOT NULL,
   orario TIME NOT NULL,
   ritardo TIME DEFAULT '00:00:00',
-  stato_volo VARCHAR(50) NOT NULL,
-  tipo_volo VARCHAR(50) NOT NULL,
-  aeroporto_origine VARCHAR(255) DEFAULT 'Napoli',
-  aeroporto_destinazione VARCHAR(255) DEFAULT 'Napoli',
+  stato_volo VARCHAR(100) NOT NULL,
+  tipo_volo VARCHAR(100) NOT NULL,
+  aeroporto_origine VARCHAR(100) DEFAULT 'Napoli',
+  aeroporto_destinazione VARCHAR(100) DEFAULT 'Napoli',
   idGateFK INTEGER
 );
-
 
 -- Definizione della tabella GESTIONE (Tabella di associazione N:M tra UTENTE e VOLO)
 CREATE TABLE gestione (
